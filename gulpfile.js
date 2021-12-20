83a3f46ee4e9fc319ddea3906ce90f421103e4fd
Denys Skorov <denys.skorov@gmail.com>
2021-12-20 12:32:07 +0200

    init repo, set up min gulp config

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const {src, dest, watch, series, parallel} = require('gulp')
+// const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug')
+const connect = require('gulp-connect');
+const sourcemaps = require('gulp-sourcemaps');
+
+
+const appPath = {
+    scss: './app/scss/**/*.scss',
+    pug: './app/index.pug',
+}
+
+const destPath = {
+    css: './dest/css',
+    html: './dest',
+}
+
+function buildStyles() {
+    return src(appPath.scss)
+        //TODO: ініціалізація карти
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            //TODO: параметри для зжимання стилів
+            // outputStyle: 'nested'
+            // outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        //TODO: запис карти
+        .pipe(sourcemaps.write())
+        .pipe(dest(destPath.css))
+        .pipe(connect.reload());
+}
+
+function buildHtml() {
+    return src(appPath.pug)
+        .pipe(pug({
+            //TODO: параметри для формування HTML
+            pretty: true
+        }))
+        .pipe(dest(destPath.html))
+        .pipe(connect.reload());
+}
+
+function startLocalServer() {
+    connect.server({
+        root: 'dest',
+        port: 8080,
+        livereload: true
+    })
+}
+
+function watchCode() {
+    watch(appPath.scss, buildStyles)
+    watch(appPath.pug, buildHtml)
+}
+
+exports.default = series(buildStyles, buildHtml, parallel(startLocalServer, watchCode))
