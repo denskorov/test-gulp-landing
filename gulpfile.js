4fae72f8ce3570ed9fba55734be6d7ace6d9c2e1
Denys Skorov <denys.skorov@gmail.com>
2021-12-23 13:47:59 +0200

    reverse dest path

--- gulpfile.js
+++ gulpfile.js
@@ -17,11 +17,11 @@ const appPath = {
 }
 
 const destPath = {
-    css: './css',
-    html: './',
-    img: './images',
-    fonts: './fonts',
-    js: './js'
+    css: './dest/css',
+    html: './dest',
+    img: './dest/images',
+    fonts: './dest/fonts',
+    js: './dest/js'
 }
 
 const jsPath = [
